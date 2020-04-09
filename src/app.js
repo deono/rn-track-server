1ba11d4b04fe842c793b0b2b37820f42c69de966
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-09 11:08:34 +0100

    create and save a new user

--- src/app.js
+++ src/app.js
@@ -1,3 +1,4 @@
+require("./models/Users");
 const express = require("express");
 require("dotenv").config();
 const mongoose = require("mongoose");
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
+
 const router = express.Router();
 
-router.post("/signup", (req, res) => {
+router.post("/signup", async (req, res) => {
   // handle signup process
-  console.log("/signup", req.body);
+  const { email, password } = req.body;
+  // create a new user
+  const user = new User({ email, password });
+  // saving the user is an async operation
+  await user.save();
+
   res.send("you made a post request to /signup");
 });
 
