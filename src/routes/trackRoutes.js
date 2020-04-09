b9f6026a811ae1c71d403c7f3e7501e3d3a4b46a
Deon Odendaal <deon.odendaal@protonmail.com>
2020-04-09 17:37:59 +0100

    adding new track route

--- src/routes/trackRoutes.js
+++ src/routes/trackRoutes.js
@@ -6,6 +6,10 @@ const router = express.Router();
 // all routes in this router require the user to be signed in
 router.use(requireAuth);
 
+/**
+ * LIST TRACKS ROUTE
+ * METHOD: GET
+ */
 router.get("/tracks", async (req, res) => {
   // get all the tracks related to the user
   // will be added in the tracks identifier in an array
@@ -13,6 +17,37 @@ router.get("/tracks", async (req, res) => {
 
   // return the tracks array
   res.status(200).json({ payload: tracks });
+});
+
+/**
+ * ADD NEW TRACK ROUTE
+ * METHOD: POST
+ */
+router.post("/tracks", async (req, res) => {
+  // assume the mobile device is going to send the data in the
+  // same object shape as the tracks model
+  const { name, locations } = req.body;
+
+  // validation
+  if (!name || !locations) {
+    return res
+      .status(422)
+      .json({ message: "You must provide a name and locations" });
+  }
+
+  try {
+    // create a new instance of a Track
+    const track = new Track({ name, locations, userId: req.user._id });
+    // save the new track to the database
+    await track.save();
+
+    // send a response
+    res.status(200).json({ payload: track });
+  } catch (error) {
+    // if something went wrong during the save process
+    // send back an error message
+    res.status(422).json({ message: err.message });
+  }
 });
 
 module.exports = router;
